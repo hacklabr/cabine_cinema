05bd614df45b9cdf1a22ab885755d5af469b456b
Fernando Silva <liquuid@hacklab.com.br>
2012-03-12 17:51:52 -0300

    adicionando estrelas, novos fixtures e ordenação dentro das select boxes

--- django/cabine/menu/admin.py
+++ django/cabine/menu/admin.py
@@ -1,8 +1,34 @@
 from django.contrib import admin
-from menu.models import Clip, Director, Country, Year, Genre
+from menu.models import Clip, Director, Country, Year, Genre, Star
 
-admin.site.register(Clip)
-admin.site.register(Director)
-admin.site.register(Country)
-admin.site.register(Year)
-admin.site.register(Genre)
+class ClipAdmin(admin.ModelAdmin):
+    list_display = ('name','orig_name', 'sinopse', 'year', 'country')
+    ordering = ('name',)
+
+class DirectorAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    ordering = ('name',)
+
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    ordering = ('name',)
+
+class YearAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    ordering = ('name',)
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    ordering = ('name',)
+
+class StarAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    ordering = ('name',)
+
+
+admin.site.register(Clip, ClipAdmin)
+admin.site.register(Director, DirectorAdmin)
+admin.site.register(Country, CountryAdmin)
+admin.site.register(Year, YearAdmin)
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(Star, StarAdmin)
--- django/cabine/menu/models.py
+++ django/cabine/menu/models.py
@@ -1,5 +1,6 @@
 # _*_ coding: UTF-8 _*_
 from django.db import models
+from django.forms import ModelForm
 
 class Clip(models.Model):
     name = models.CharField(verbose_name='Título do filme', max_length = 300)
@@ -11,31 +12,56 @@ class Clip(models.Model):
     director = models.ManyToManyField('Director', null=True, blank=True, verbose_name='Diretor')
     country = models.ForeignKey('Country', null=True, blank=True, verbose_name='País')
     year = models.ForeignKey('Year',null=True, blank=True, verbose_name='Ano de lançamento')
-
+    star = models.ManyToManyField('Star', null=True, blank=True, verbose_name='Estrelas')
+    
     def __unicode__(self):
         return self.name
 
-
+class Star(models.Model):
+    name = models.CharField(verbose_name='Atores', max_length = 300)
+    
+    class Meta:
+        ordering = ('name',)
+        
+    def __unicode__(self):
+        return self.name
+    
 class Genre(models.Model):
     name = models.CharField(verbose_name='Gênero', max_length = 300)
     
+    class Meta:
+        ordering = ('name',)
+            
+
     def __unicode__(self):
         return self.name
 
 class Director(models.Model):
     name = models.CharField(verbose_name='Diretor', max_length = 300)
     
+    class Meta:
+        ordering = ('name',)
+        
+
     def __unicode__(self):
         return self.name
 
 class Country(models.Model):
     name = models.CharField(verbose_name='País', max_length = 300)
     
+    class Meta:
+        ordering = ('name',)
+        
+
     def __unicode__(self):
         return self.name
 
 class Year(models.Model):
     name = models.CharField(verbose_name='Ano', max_length = 300)
+    
+    class Meta:
+        ordering = ('name',)
+        
     
     def __unicode__(self):
         return self.name
