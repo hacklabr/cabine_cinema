032360590366b26e40d350e1f0dae9b6bc61c44f
Fernando Silva <liquuid@liquuid.net>
2012-04-27 13:43:35 -0300

    novas entradas no player

--- player/player.sh
+++ player/player.sh
@@ -3,5 +3,5 @@ rm -rf /tmp/fifo
 rm -rf /tmp/waiting
 
 mkfifo /tmp/fifo
-
-mplayer ~/COUNTDOWN.mp4 -fs -quiet -idle -fixed-vo -slave -input file=/tmp/fifo > /tmp/mplayer.log
+export DISPLAY=:0.1
+mplayer ~/COUNTDOWN.mp4 -fs -quiet -idle -fixed-vo -monitoraspect 16:9 -slave -input file=/tmp/fifo > /tmp/mplayer.log
